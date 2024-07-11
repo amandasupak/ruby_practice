2efa08a10d3addef22641d53c7a8d04ba47a9874
AmandaSupak <Amandasupak@gmail.com>
2024-07-11 17:53:05 -0500

    starting problem 20

---
+++ Easy_problems/020_valid_parenthesis.rb
@@ -0,0 +1,22 @@
+# Problem 20:
+# https://leetcode.com/problems/valid-parentheses/description/
+# Given a string s containing just the characters 
+# '(', ')', '{', '}', '[' and ']', 
+# determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+
+
+def is_valid(str)
+  
+end
+
+
+test_is_valid("()")
+test_is_valid("(){}[]")
+test_is_valid("(]")
