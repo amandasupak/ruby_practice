4d405dd13133feb1d0e0caa831a67bc2e1319917
AmandaSupak <Amandasupak@gmail.com>
2024-07-12 00:15:56 -0500

    finished 20

--- Easy_problems/020_valid_parenthesis.rb
+++ Easy_problems/020_valid_parenthesis.rb
@@ -13,10 +13,33 @@
 
 
 def is_valid(str)
+  stack = []
+  matching_bracket = { '(' => ')', '[' => ']', '{' => '}' }
+
+  str.each_char do |char|
+    if matching_bracket.keys.include?(char) #if there is an open bracket
+      stack.push(char)
+    elsif matching_bracket.values.include?(char) #if there is a close bracket
+      if stack.empty? || matching_bracket[stack.pop] != char 
+        return false
+      end
+    end
+  end
   
+  stack.empty?
+end
+
+def test_is_valid(str)
+puts "Input: #{str}"
+results = is_valid(str)
+puts "#{results.inspect}"
+puts "" # Just for a clear separation in outputs
 end
 
 
-test_is_valid("()")
-test_is_valid("(){}[]")
-test_is_valid("(]")
+test_is_valid("()")       # output: true
+test_is_valid("(){}[]")   # output: true
+test_is_valid("({})")     # output: true
+test_is_valid("(]")       # output: false
+test_is_valid("({])")     # output: false
+test_is_valid("(apple)")  # output: false
