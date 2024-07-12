d3371398016e1fa2ab56c3b453776885d0ebdac9
AmandaSupak <Amandasupak@gmail.com>
2024-07-12 10:19:39 -0500

    added comments 20

--- Easy_problems/020_valid_parenthesis.rb
+++ Easy_problems/020_valid_parenthesis.rb
@@ -16,14 +16,29 @@ def is_valid(str)
   stack = []
   matching_bracket = { '(' => ')', '[' => ']', '{' => '}' }
 
-  str.each_char do |char|
-    if matching_bracket.keys.include?(char) #if there is an open bracket
-      stack.push(char)
-    elsif matching_bracket.values.include?(char) #if there is a close bracket
+  # Build a stack when you find an open bracket, and remove it from the stack
+  # when you find a cooresponding closing bracket. 
 
-      #return false if either of these are true
-      ### if the stack is empty then there is no open bracket before it
-      if stack.empty? || matching_bracket[stack.pop] != char 
+  str.each_char do |char|
+
+    # if there is an open bracket, add char to the stack
+    if matching_bracket.keys.include?(char) 
+      stack.push(char)
+
+    # if there is a close bracket
+    elsif matching_bracket.values.include?(char) 
+
+      # Return false if either of these are true
+      ### 1) If the stack is empty then there is no open bracket before it, so 
+      ### there is no match for the closing bracket
+      ### 2) If you pop off the stack and find that it's opposite does not match 
+      ### the char, return true for the if statement. Example: Your char is a ')'
+      ### You pop off the stack an '[' then find it's hash value ']'. These two 
+      ### are not equal to each other and the if statement would return true, which
+      ### then returns false and ends the loop. If the stack was a '(' with a has 
+      ### of ')', and the char was a ')' then thses two would match and return false
+      ### for the if statement and continue the loop. 
+      if stack.empty? || matching_bracket[stack.pop] != char
         return false
       end
     end
